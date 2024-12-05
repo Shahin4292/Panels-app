41affd027b7c1498d4fb443de1ee1a91a8539695
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-05 18:51:45 +0600

    done

--- lib/widget/image_tile.dart
+++ lib/widget/image_tile.dart
@@ -13,6 +13,39 @@ class ImageTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(imageSource);
+    return Container(
+        height: extent,
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.network(
+              imageSource,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                  padding: EdgeInsets.all(8),
+                  color: Colors.black.withOpacity(0.7),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Art $index',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      InkWell(child: Icon(Icons.favorite_outline))
+                    ],
+                  )),
+            ),
+          ],
+        ));
   }
 }
