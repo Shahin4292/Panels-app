01a483abee6a1839941844348266ff05e2cd6dbe
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-13 09:12:11 +0600

    added nested Scrolled

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+import '../widget/image_tile.dart';
 
 class ExplorePage extends StatefulWidget {
   const ExplorePage({super.key});
@@ -10,10 +13,24 @@ class ExplorePage extends StatefulWidget {
 class _ExplorePageState extends State<ExplorePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Explore Page"),
+    return Scaffold(
+        body: NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) {
+        return [];
+      },
+      body: MasonryGridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        padding: EdgeInsets.all(10),
+        itemBuilder: (context, index) {
+          return ImageTile(
+            index: index,
+            imageSource: 'https://picsum.photos/500/500?random=$index',
+            extent: (index % 2) == 0 ? 300 : 150,
+          );
+        },
       ),
-    );
+    ));
   }
 }
