c3f78241cdadfbb9607d566dd21221e020646397
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-14 10:29:57 +0600

    add slide to page change

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -12,6 +12,8 @@ class ExplorePage extends StatefulWidget {
 }
 
 class _ExplorePageState extends State<ExplorePage> {
+  var _selectedSlideIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,29 +23,57 @@ class _ExplorePageState extends State<ExplorePage> {
           SliverAppBar(
             expandedHeight: 320,
             flexibleSpace: FlexibleSpaceBar(
-              background: PageView.builder(
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return Stack(
-                      fit: StackFit.expand,
-                      children: [
-                        CachedNetworkImage(
-                          imageUrl:
-                              'https://picsum.photos/500/500?random=slide_$index',
-                          fit: BoxFit.cover,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                stops: [0.01, 1],
-                                begin: Alignment.bottomCenter,
-                                end: Alignment.topCenter,
-                                colors: [Colors.black, Colors.transparent]),
+              background: Stack(
+                children: [
+                  PageView.builder(
+                    onPageChanged: (value) {
+                      setState(() {
+                        _selectedSlideIndex = value;
+                      });
+                    },
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          CachedNetworkImage(
+                            imageUrl:
+                                'https://picsum.photos/500/500?random=slide_$index',
+                            fit: BoxFit.cover,
                           ),
-                        )
-                      ],
-                    );
-                  }),
+                          Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  stops: [0.01, 1],
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                  colors: [Colors.black, Colors.transparent]),
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                  Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: 20,
+                      child: Wrap(
+                          alignment: WrapAlignment.center,
+                          children: List.generate(5, (index) {
+                            return Container(
+                              height: 12,
+                              width: 12,
+                              margin: EdgeInsets.symmetric(horizontal: 8),
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: index == _selectedSlideIndex
+                                      ? Colors.white
+                                      : Colors.grey),
+                            );
+                          })))
+                ],
+              ),
             ),
           ),
           SliverAppBar(),
