b7ce693b895fb7b64d9f5080370277d1d7d22456
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-12-15 17:11:21 +0600

    add bottom sheet

--- lib/widget/image_tile.dart
+++ lib/widget/image_tile.dart
@@ -14,48 +14,70 @@ class ImageTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: extent,
-        clipBehavior: Clip.antiAlias,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            CachedNetworkImage(
-              imageUrl: imageSource,
-              fit: BoxFit.cover,
-            ),
-            // Image.network(
-            //   imageSource,
-            //   fit: BoxFit.cover,
-            // ),
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: Container(
-                  padding: EdgeInsets.all(8),
-                  color: Colors.black.withOpacity(0.7),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Art $index',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: () {
+        showImageDetails(context imageSource);
+      },
+      child: Container(
+          height: extent,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              CachedNetworkImage(
+                imageUrl: imageSource,
+                fit: BoxFit.cover,
+              ),
+              // Image.network(
+              //   imageSource,
+              //   fit: BoxFit.cover,
+              // ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                    padding: EdgeInsets.all(8),
+                    color: Colors.black.withOpacity(0.7),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Art $index',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
-                      ),
-                      InkWell(
-                        child: Icon(
-                          Icons.favorite_outline,
-                          color: Colors.white,size: 20,
+                        InkWell(
+                          child: Icon(
+                            Icons.favorite_outline,
+                            color: Colors.white,size: 20,
+                          ),
                         ),
-                      ),
-                    ],
-                  )),
-            ),
-          ],
-        ));
+                      ],
+                    )),
+              ),
+              Column(
+
+                children: [
+
+              ],)
+            ],
+          )),
+    );
+  }
+
+  void showImageDetails(BuildContext context, String imageSource) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+        context: context,
+        builder: (context){
+      return FractionallySizedBox(
+
+        heightFactor: 0.9,
+      );
+    });
   }
 }
